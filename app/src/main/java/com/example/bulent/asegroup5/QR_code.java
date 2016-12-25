0f278aa5d0b923abf93b7260ed1fb0378b746fab
bulenthacioglu <bulent.hacioglu@hotmail.com>
2016-12-25 17:50:03 +0100

    log out functionality

--- app/src/main/java/com/example/bulent/asegroup5/QR_code.java
+++ app/src/main/java/com/example/bulent/asegroup5/QR_code.java
@@ -1,7 +1,9 @@
 package com.example.bulent.asegroup5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class QR_code extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class QR_code extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code);
+    }
+    public void generateQR(View view) {
+        // Do something in response to button
+        Intent myIntent = new Intent(QR_code.this, MainActivity.class);
+        QR_code.this.startActivity(myIntent);
+
+
     }
 }
